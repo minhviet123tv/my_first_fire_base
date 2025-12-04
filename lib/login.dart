1fb72c44a1f427f5309ddd5d8201c1518ff0a671
minhviet123tv <136576401+minhviet123tv@users.noreply.github.com>
2025-12-04 10:06:13 +0700

    1

--- lib/login.dart
+++ lib/login.dart
@@ -53,163 +53,161 @@ class _LogInState extends State<LogIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset(
-                  "images/car.PNG",
-                  fit: BoxFit.cover,
-                )),
-            SizedBox(
-              height: 30.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-              child: Form(
-                key: _formkey,
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 30.0),
-                      decoration: BoxDecoration(color: Color(0xFFedf0f8), borderRadius: BorderRadius.circular(30)),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please Enter E-mail';
-                          }
-                          return null;
-                        },
-                        controller: mailcontroller,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Email",
-                            hintStyle: TextStyle(color: Color(0xFFb2b7bf), fontSize: 18.0)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 30.0),
-                      decoration: BoxDecoration(color: Color(0xFFedf0f8), borderRadius: BorderRadius.circular(30)),
-                      child: TextFormField(
-                        controller: passwordcontroller,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please Enter Password';
-                          }
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Password",
-                            hintStyle: TextStyle(color: Color(0xFFb2b7bf), fontSize: 18.0)),
-                        obscureText: true,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        if (_formkey.currentState!.validate()) {
-                          setState(() {
-                            email = mailcontroller.text;
-                            password = passwordcontroller.text;
-                          });
+      body: Column(
+        children: [
+          SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                "images/car.PNG",
+                fit: BoxFit.cover,
+              )),
+          SizedBox(
+            height: 30.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 30.0),
+                    decoration: BoxDecoration(color: Color(0xFFedf0f8), borderRadius: BorderRadius.circular(30)),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter E-mail';
                         }
-                        userLogin();
+                        return null;
                       },
-                      child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          padding: EdgeInsets.symmetric(vertical: 13.0, horizontal: 30.0),
-                          decoration: BoxDecoration(color: Color(0xFF273671), borderRadius: BorderRadius.circular(30)),
-                          child: Center(
-                              child: Text(
-                            "Sign In",
-                            style: TextStyle(color: Colors.white, fontSize: 22.0, fontWeight: FontWeight.w500),
-                          ))),
+                      controller: mailcontroller,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Email",
+                          hintStyle: TextStyle(color: Color(0xFFb2b7bf), fontSize: 18.0)),
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 30.0),
+                    decoration: BoxDecoration(color: Color(0xFFedf0f8), borderRadius: BorderRadius.circular(30)),
+                    child: TextFormField(
+                      controller: passwordcontroller,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter Password';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          hintStyle: TextStyle(color: Color(0xFFb2b7bf), fontSize: 18.0)),
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      if (_formkey.currentState!.validate()) {
+                        setState(() {
+                          email = mailcontroller.text;
+                          password = passwordcontroller.text;
+                        });
+                      }
+                      userLogin();
+                    },
+                    child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.symmetric(vertical: 13.0, horizontal: 30.0),
+                        decoration: BoxDecoration(color: Color(0xFF273671), borderRadius: BorderRadius.circular(30)),
+                        child: const Center(
+                            child: Text(
+                          "Sign In",
+                          style: TextStyle(color: Colors.white, fontSize: 22.0, fontWeight: FontWeight.w500),
+                        ))),
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ForgotPassword()));
-              },
-              child: Text("Forgot Password?",
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ForgotPassword()));
+            },
+            child: Text("Forgot Password?",
+                style: TextStyle(color: Color(0xFF8c8e98), fontSize: 18.0, fontWeight: FontWeight.w500)),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Text(
+            "or LogIn with",
+            style: TextStyle(color: Color(0xFF273671), fontSize: 22.0, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  // AuthMethods().signInWithGoogle(context);
+                },
+                child: Image.asset(
+                  "images/google.png",
+                  height: 45,
+                  width: 45,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                width: 30.0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  // AuthMethods().signInWithApple();
+                },
+                child: Image.asset(
+                  "images/apple1.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Don't have an account?",
                   style: TextStyle(color: Color(0xFF8c8e98), fontSize: 18.0, fontWeight: FontWeight.w500)),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Text(
-              "or LogIn with",
-              style: TextStyle(color: Color(0xFF273671), fontSize: 22.0, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // AuthMethods().signInWithGoogle(context);
-                  },
-                  child: Image.asset(
-                    "images/google.png",
-                    height: 45,
-                    width: 45,
-                    fit: BoxFit.cover,
-                  ),
+              SizedBox(
+                width: 5.0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp()));
+                },
+                child: Text(
+                  "SignUp",
+                  style: TextStyle(color: Color(0xFF273671), fontSize: 20.0, fontWeight: FontWeight.w500),
                 ),
-                SizedBox(
-                  width: 30.0,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // AuthMethods().signInWithApple();
-                  },
-                  child: Image.asset(
-                    "images/apple1.png",
-                    height: 50,
-                    width: 50,
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account?",
-                    style: TextStyle(color: Color(0xFF8c8e98), fontSize: 18.0, fontWeight: FontWeight.w500)),
-                SizedBox(
-                  width: 5.0,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp()));
-                  },
-                  child: Text(
-                    "SignUp",
-                    style: TextStyle(color: Color(0xFF273671), fontSize: 20.0, fontWeight: FontWeight.w500),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
